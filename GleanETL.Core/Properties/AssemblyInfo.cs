fb58066756affe11850fcdf09397013e41187afe
fallenidol <paul@mcilreavy.com>
2015-02-25 11:08:13 +1000

    update read and add AssemblyInfo

---
+++ GleanETL.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GleanETL")]
+[assembly: AssemblyDescription("Aims to be a simple framework with which to extract data from text files, particularly if they are in non-standard formats.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paul Mcilreavy")]
+[assembly: AssemblyProduct("GleanETL")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("973d2492-d176-4b26-b0a1-3edf36cbd486")]
+
+// Will be replaced by AppVeyor
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Gleanio
-=======
+GleanETL
+========
 [![Build status](https://ci.appveyor.com/api/projects/status/wcgnmg6ob67t69aw/branch/master?svg=true)](https://ci.appveyor.com/project/fallenidol/gleanetl/branch/master)
 
-Makes extracting data from text files that are in difficult formats a snap. Aims to provide a flexible ETL framework with a focus on performance.
+Makes extracting data from text files that are in difficult formats a snap. Aims to provide a flexible [ETL](https://en.wikipedia.org/wiki/Extract,_transform,_load) framework with a focus on performance.
